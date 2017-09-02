84626818882b5e78902788477080076a31e5d5ac
mgol <michal.golabek94@gmail.com>
2017-09-02 22:48:52 +0200

    added custom styles and UI mockup

--- dev/index.js
+++ dev/index.js
@@ -1,3 +1,4 @@
 require("material-design-lite/material.min.js");
 require("material-design-lite/material.min.css");
 require("material-design-lite/dist/material.amber-light_blue.min.css");
+require("./style.scss");
